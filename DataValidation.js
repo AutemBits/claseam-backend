1f24042ac9cbf59e4247a52d663444de27e4edae
dani9oo <dani9oo.dev@gmail.com>
2020-02-25 21:59:32 -0600

    Added user data validation logic using joi

---
+++ DataValidation.js
@@ -0,0 +1,21 @@
+// Library required to make validations in JavaScript
+
+const Joi = require('@hapi/joi');
+
+module.exports = {
+    // Function to validate user data
+    user = data => {
+        let schema = {
+            email: Joi.string().email().required(),
+            /*
+            *   Passwords must have the following requirements:
+            *       - Be at least 6 characters long
+            *       - Have at least 1 lowercase letter
+            *       - Have at least 1 uppercase letter
+            *       - Have at least 1 number 
+            */
+            password: Joi.string().regex(new RegExp("^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])")).min(6).required()
+        }
+        return Joi.validate(data, schema);
+    }
+};
