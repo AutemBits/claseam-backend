f87593fd64cdef7be026f7720dad323a463b379c
dani9oo <dani9oo.dev@gmail.com>
2020-02-25 22:02:41 -0600

    Reworked UserModel data definition

--- app/models/UserModel.js
+++ app/models/UserModel.js
@@ -1,12 +1,15 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+// Definition of the user schema
+
 const UserSchema = new Schema ({
-    dropboxid : String,
-    nombre : String,
-    correo : String,
-    grupo : String,
-    calificaciones : Array
+    forename : String,
+    surname : String,
+    email : String,
+    password: String,
+    group : String,
+    grades : Array
 });
 
 module.exports = mongoose.model('user', UserSchema);
