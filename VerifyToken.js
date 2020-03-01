6ffd21840bdff96f5de79484fe397bfb32081967
dani9oo <dani9oo.dev@gmail.com>
2020-03-01 00:39:46 -0600

    Implemented Authorization Added function to authenticate super user Fixed private key not being read

--- VerifyToken.js
+++ VerifyToken.js
@@ -1,5 +1,6 @@
 const jwt = require('jsonwebtoken');
 const fs = require('fs');
+const privatekey = fs.readFileSync('./claseam.key');
 
 module.exports = {
     auth: (req, res, next) => {
@@ -7,9 +8,22 @@ module.exports = {
         if(!token) return res.json({ success: false, result: "Access denied", code: 401});
 
         try {
-            let privatekey = fs.readFileSync('../../claseam.pem');
-            let verify = jwt.verify(token, privatekey);
+            let verified = jwt.verify(token, privatekey);
             req.user = verified;
+            if(permission && verified.type != permission) return res.json({ success: false, result: "Access denied", code: 401});
+            next();
+        } catch (err) {
+            res.json({ success: false, result: "Invalid token", code: 400});
+        }
+    },
+    authsu: (req, res, next) => {
+        let token = req.header('auth-token');
+        if(!token) return res.json({ success: false, result: "Access denied", code: 401});
+
+        try {
+            let verified = jwt.verify(token, privatekey);
+            req.user = verified;
+            if(verified.type != 'Su') return res.json({ success: false, result: "Access denied", code: 401});
             next();
         } catch (err) {
             res.json({ success: false, result: "Invalid token", code: 400});
