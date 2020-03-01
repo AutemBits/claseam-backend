15cb3a8a9a4a24d05ac26f242ad0747060bd1ecb
dani9oo <dani9oo.dev@gmail.com>
2020-03-01 00:42:36 -0600

    Added comments for routes

--- app/routes/UserRoutes.js
+++ app/routes/UserRoutes.js
@@ -2,12 +2,28 @@ const router = require('express').Router();
 const verify = require('../../VerifyToken');
 const UserControl = require('../controllers/UserControl');
     
+//  User registration route
+
 router.post('/create', UserControl.create);
+
+/*
+*   User update route
+*
+*   TODO:
+*       Make two separate routes for updating users
+*       one should be for users to change their own
+*       data and another one for the admin to chan-
+*       ge other users information.
+*/ 
 
 router.post('/update', verify.auth, UserControl.update);
 
+//  User data retrieval route
+
 router.get('/retrieve', verify.auth, UserControl.retrieve);
 
-router.delete('/delete', verify.auth, UserControl.delete);
+//  User deletion route
+
+router.delete('/delete', verify.authsu, UserControl.delete);
 
 module.exports = router;
